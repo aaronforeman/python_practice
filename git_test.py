c20e286bffef2a63d1f3d810cbfea71c7b302f45
aaronforeman <aaron.foreman19@ou.edu>
2020-06-19 10:58:01 -0500

    Test. Again.

--- git_test.py
+++ git_test.py
@@ -1,9 +1,4 @@
 print("Hello Git!")
 print("This is a test of using git in vs code.")
 
-<<<<<<< HEAD
-print("A different kind of test.")
-=======
-print("This is a test for breaking the project!")
-prtin("Test from the GitHub website.")
->>>>>>> 5d4911bd6dccfe9df8a4d16b020183cfacd92684
+
